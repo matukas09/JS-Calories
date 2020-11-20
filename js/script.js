8f31f0c0cfba1e5c5bb22badd092e2c402a195b2
Opilane TTHK <opilane@tthk.ee>
2020-11-20 11:21:49 +0200

    yaa

--- js/script.js
+++ js/script.js
@@ -1,12 +1,26 @@
 const addButton = document.getElementById('add');
 const foodName = document.querySelector('#item-name');
 const foodCalories = document.querySelector('#item-calories');
+const totalSpan = document.querySelector('.total-calories');
+const list = document.querySelector('#item-list');
 let total = 0;
 
 addButton.addEventListener('click', (event)=> {
     let mealName = foodName.value; //get the food name from the input file
     let mealCalories = parseInt(foodCalories.value); // get the calories form the input field
     total = total + mealCalories; //calculates the total calorie intake
+    totalSpan.innerHTML = total;
+
+    let listItem = document.createElement('li');
+    listItem.classList.add('collection-item');
+    let text = `Food ${mealName}. Calories: ${mealCalories}`;
+    listItem.appendChild(document.createTextNode(text));
+    console.log(listItem);
+    list.appendChild(listItem);
+
+    //Clear the entry fields
+    foodName.value = '';
+    foodCalories.value = '';
 
     console.log('Food:', typeof(mealName));
     console.log('Calories:', typeof(mealCalories));
