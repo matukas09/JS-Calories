f21790b058105a82c1ab31cd4fa33f66e312d90e
Opilane TTHK <opilane@tthk.ee>
2020-11-20 10:49:59 +0200

    yes

--- js/script.js
+++ js/script.js
@@ -1,6 +1,16 @@
-console.log('test');
-const addButton = document.querySelector('.add-btn');
+const addButton = document.getElementById('add');
+const foodName = document.querySelector('#item-name');
+const foodCalories = document.querySelector('#item-calories');
+let total = 0;
 
-addButton.addEventListener('click', ()=> {
-    console.log('test');
+addButton.addEventListener('click', (event)=> {
+    let mealName = foodName.value; //get the food name from the input file
+    let mealCalories = parseInt(foodCalories.value); // get the calories form the input field
+    total = total + mealCalories; //calculates the total calorie intake
+
+    console.log('Food:', typeof(mealName));
+    console.log('Calories:', typeof(mealCalories));
+    console.log('Total:', total);
+
+    event.preventDefault();
 });
